2c4b194c6bcd02487d189ebcf09cb2494b1204ce
Abolfazlkhazraei <t3khab00@students.oamk.fi>
2024-03-14 08:52:59 +0200

    Spiral

---
+++ FreestyleTasks/Three.Js/script.js
@@ -0,0 +1,18 @@
+const words = "coding is super cool"; // Add more words as needed
+
+const ANIMATION_DURATION = 4000;
+
+// Separate each character into it's own div
+const characters = words.split("").forEach((char, i) => {
+    function createElement(offset) {
+        const div = document.createElement('div'); 
+        div.innerText = char;
+        div.classList.add("character");
+        div.style.animationDelay = `${i * (ANIMATION_DURATION / 16) - offset}ms`;
+
+        return div;
+    }
+
+    document.getElementById("spiral").append(createElement(0));
+    document.getElementById("spiral2").append(createElement(-1 * (ANIMATION_DURATION / 2)));
+});
